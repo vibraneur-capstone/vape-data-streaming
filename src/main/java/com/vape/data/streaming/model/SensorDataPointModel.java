b297142279b7ed1977c89aa488f98adf470d4d33
BenxinNiu <benxin.niu@mun.ca>
2020-01-22 21:37:02 -0330

    fix pmd violation

--- src/main/java/com/vape/data/streaming/model/SensorDataPointModel.java
+++ src/main/java/com/vape/data/streaming/model/SensorDataPointModel.java
@@ -5,7 +5,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
