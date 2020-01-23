95fd9eaf353d40263bc75d3b2fccbce169288ca5
BenxinNiu <benxin.niu@mun.ca>
2020-01-23 16:57:24 -0330

    refactor model to use Double instead ofg BigDecimal

--- src/main/java/com/vape/data/streaming/model/SensorDataPointModel.java
+++ src/main/java/com/vape/data/streaming/model/SensorDataPointModel.java
@@ -4,7 +4,6 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Data
@@ -19,5 +18,5 @@ public class SensorDataPointModel {
     private String sensorId;
     private String sensorHubId;
     private String timestamp;
-    private List<BigDecimal> data;
+    private List<Double> data;
 }
