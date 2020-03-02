0627cb6432ebe10a460d6207c8b01c96453e38d5
BenxinNiu <benxin.niu@mun.ca>
2020-03-02 16:00:38 -0330

    add query for dsp data

--- src/main/java/com/vape/data/streaming/repository/DspDataPointModelRepository.java
+++ src/main/java/com/vape/data/streaming/repository/DspDataPointModelRepository.java
@@ -2,6 +2,13 @@ package com.vape.data.streaming.repository;
 
 import com.vape.data.streaming.model.DspDataPointModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface DspDataPointModelRepository extends MongoRepository<DspDataPointModel, String> {
+
+    @Query(value = "{sensorId: ?0, 'timestamp': {$gte: ?1, $lte:?2 }}", sort = "{'timestamp': 1}")
+    List<DspDataPointModel> findDspDataBySensorAndDateRange(String id, String from, String to);
+
 }
