ca0d5ea3e7a8a74302cd3377667664b9030b1cb9
BenxinNiu <benxin.niu@mun.ca>
2020-03-02 16:00:03 -0330

    implemented new controller from swagger

---
+++ src/main/java/com/vape/data/streaming/controller/DspDataController.java
@@ -0,0 +1,41 @@
+package com.vape.data.streaming.controller;
+
+import com.vape.data.streaming.mapper.DspDataPointModelMapper;
+import com.vape.data.streaming.model.DspDataPointModel;
+import com.vape.data.streaming.service.DspDataQueryService;
+import com.vape.data.streaming.swagger.v1.api.DspApi;
+import com.vape.data.streaming.swagger.v1.model.DspDataList;
+import com.vape.data.streaming.swagger.v1.model.DspDataStatus;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin()
+@AllArgsConstructor
+@Slf4j
+public class DspDataController implements DspApi{
+
+    private final DspDataQueryService dspDataQueryService;
+    private final DspDataPointModelMapper dspDataPointModelMapper;
+
+    @Override
+    public ResponseEntity<DspDataList> dspDataGet(String sensorId, Integer from, Integer to){
+        List<DspDataPointModel> dspDataPointModelList = dspDataQueryService.getDataBySensorAndDateRange(sensorId, from, to);
+        DspDataList dspDataList = dspDataPointModelMapper.toDspDataList(dspDataPointModelList);
+        dspDataList.setFrom(from);
+        dspDataList.setTo(to);
+        dspDataList.setSensorId(sensorId);
+        return new ResponseEntity<>(dspDataList, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<DspDataStatus> dspDataStatusGet(Integer from, Integer to){
+        return new ResponseEntity<>(new DspDataStatus(), HttpStatus.OK);
+    }
+}
